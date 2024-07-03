7be22604f15015a6ed7bf3918a29321ea26be2d6
Matthew McClain <mthommcclain@gmail.com>
2024-07-03 16:50:09 -0700

    add rename abililty, wire back in KL saving

--- src/components/Files.vue
+++ src/components/Files.vue
@@ -2,7 +2,7 @@
     <button @click="upload">Upload</button>
     <div id="card-container">
         <div v-for="info, id in data.files" class="card-outer">
-            <span>{{ info.name }}</span>
+            <span @click="updateName(id)">{{ info.name }}</span>
             <Card
                 :info="info"
                 :id="id"
@@ -31,21 +31,34 @@ Agent
                 const { active_type, name } = await Agent.metadata(id)
                 data.files[id] = { url, name, type: active_type}
             })
+        data.uploadedFiles = state // ref to sync uuids to KL run-state
     })
 
 async function upload() {
     const id = await Agent.upload({ browser: true, validate })
     if (!id) alert('file not uploaded, likely too large')
     if (id) {
+        data.uploadedFiles[id] = {} // add uuid to KL run-state
         const url = await Agent.download(id).url()
         const { active_type, name } = await Agent.metadata(id)
-        data.files[id] = { url, name, type: active_type}
+        data.files[id] = { url, name, type: active_type }
+
     }
     else console.log('No file selected....')
 }
 
+async function updateName(id) {
+    const newName = prompt('Enter New Name')?.trim()
+    if (newName) {
+        const x = await Agent.metadata(id)
+        x.name = newName
+        data.files[id].name = newName
+    }
+}
+
 function removeFile(id) {
-    delete data.files[id]
+    delete data.files[id] // remove local
+    delete data.uploadedFiles[id] // remove from KL run-state
 }
 
 </script>
@@ -67,5 +80,9 @@ function removeFile(id) {
 .card-outer span {
     font-size: 0.8rem;
     margin-bottom: 2px;
+    cursor: pointer;
+}
+.card-outer span:hover {
+    color: blue;
 }
 </style>
