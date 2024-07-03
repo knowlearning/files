bf275aa9ed97de42059dadd656ff498d119a1d46
Matthew McClain <mthommcclain@gmail.com>
2024-07-03 15:11:39 -0700

    isolate card component

---
+++ src/components/Card.vue
@@ -0,0 +1,112 @@
+<template>
+    <div
+        class="card"
+        draggable="true"
+        @dragstart="setDragData($event, props.id)"
+    >
+        <img
+            v-if="info.type && info.type.startsWith('image')"
+            draggable="false"
+            :src="info.url"
+        />
+
+        <video controls v-else-if="info.type && info.type.startsWith('video')">
+            <source :src="info.url" :type="info.type">
+                Your browser does not support the video element.
+        </video>
+
+        <audio controls   v-else-if="info.type && info.type.startsWith('audio')">
+            <source :src="info.url" :type="info.type">
+            Your browser does not support the audio element.
+        </audio>
+
+        <div
+            class="card-remove-button"
+            @click="emits('remove',id)"
+        >
+            &#x274C;
+        </div>
+        <div
+            class="card-copy-button"
+            @click="copyToClipboard(id)"
+        >
+            &#x2398;
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+const emits = defineEmits(['remove'])
+const props = defineProps(['id', 'info'])
+const info = reactive(props.info)
+
+function setDragData(event, id) {
+    event.dataTransfer.setData('text/plain', id)
+    event.dataTransfer.setData('text/uri-list', id)
+}
+
+function copyToClipboard(text) {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+}
+
+</script>
+
+<style scoped>
+.card
+{
+    position: relative;
+    background: #f9f9f9;
+    width: 200px;
+    height: 120px;
+    margin: 16px;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    align-items: center;
+    justify-content: center;
+    border-radius: 4px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+}
+.card:hover
+{
+    cursor: grab;
+}
+.card>img
+{
+    max-width: 90%;
+    max-height: 90%;
+}
+.card-remove-button,
+.card-copy-button
+{
+    opacity: 0.1;
+    position: absolute;
+    bottom: 4px;
+    cursor: pointer;
+    animation: 300ms ;
+}
+.card-remove-button {
+    right: 8px;
+}
+.card-copy-button {
+    right: 32px;
+    bottom: -2px;
+    font-size: 2rem;
+}
+.card-remove-button:hover,
+.card-copy-button:hover
+{
+    opacity: 1;
+}
+video {
+    width: 200px;
+    height: 150px;
+}
+audio { width: 200px; }
+</style>
--- src/components/Files.vue
+++ src/components/Files.vue
@@ -1,150 +1,56 @@
 <template>
-  <button @click="uploadNew">Upload New</button>
-  <div id="card-container">
-    <div
-      class="card"
-      v-for="{ downloadUrl, type }, id in data.files"
-      draggable="true"
-      @dragstart="setDragData($event, id)"
-    >
-      <img
-        v-if="type && type.startsWith('image')"
-        draggable="false"
-        :src="downloadUrl"
-      />
-
-      <video controls v-else-if="type && type.startsWith('video')">
-        <source :src="downloadUrl" :type="type">
-          Your browser does not support the video element.
-      </video>
-
-      <audio controls   v-else-if="type && type.startsWith('audio')">
-        <source :src="downloadUrl" :type="type">
-        Your browser does not support the audio element.
-      </audio>
-
-      <div
-        class="card-remove-button"
-        @click="removeFile(id)"
-      >
-        &#x274C;
-      </div>
-      <div
-        class="card-copy-button"
-        @click="copyToClipboard(id)"
-      >
-        &#x2398;
-      </div>
+    <button @click="uploadNew">Upload New</button>
+    <div id="card-container">
+        <Card
+            v-for="info, id in data.files"
+            :info="info"
+            :id="id"
+            @remove="removeFile"
+        />
     </div>
-  </div>
 </template>
 
 <script setup>
+import Card from './Card.vue'
 import { reactive } from 'vue'
 
 const data = reactive({
-  files: {}
+    files: {}
 })
 
 Agent
-  .state('uploaded-files')
-  .then(state => {
-    Object
-      .keys(state)
-      .forEach(async id => {
-        data.files[id] = { downloadUrl: null, type: null }
-        data.files[id].downloadUrl = await Agent.download(id).url()
-        data.files[id].type = (await Agent.metadata(id)).active_type
-      })
-    data.uploadedFiles = state
-  })
+    .state('uploaded-files')
+    .then(state => {
+        Object
+            .keys(state)
+            .forEach(async id => {
+                const url = await Agent.download(id).url()
+                const { active_type, name } = await Agent.metadata(id)
+                data.files[id] = { url, name, type: active_type}
+            })
+    })
 
 async function uploadNew() {
-  const id = await Agent.upload({ browser: true })
-
-  if (id) {
-    data.uploadedFiles[id] = {}
-    data.files[id] = { downloadUrl: null }
-    data.files[id].downloadUrl = await Agent.download(id).url()
-  }
-  else console.log('No file selected....')
-}
-
-function setDragData(event, id) {
-  event.dataTransfer.setData('text/plain', id)
-  event.dataTransfer.setData('text/uri-list', id)
+    const id = await Agent.upload({ browser: true })
+    if (id) {
+        const url = await Agent.download(id).url()
+        const { active_type, name } = await Agent.metadata(id)
+        data.files[id] = { url, name, type: active_type}
+    }
+    else console.log('No file selected....')
 }
 
 function removeFile(id) {
-  delete data.files[id]
-  delete data.uploadedFiles[id]
-}
-function copyToClipboard(text) {
-    const textarea = document.createElement('textarea');
-    textarea.value = text;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textarea);
+    delete data.files[id]
 }
 
 </script>
 
-
-
 <style scoped>
-  #card-container
-  {
+#card-container
+{
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-  }
-  .card
-  {
-    position: relative;
-    background: #f9f9f9;
-    width: 200px;
-    height: 120px;
-    margin: 16px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 4px;
-    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-  }
-  .card:hover
-  {
-    cursor: grab;
-  }
-  .card>img
-  {
-    max-width: 90%;
-    max-height: 90%;
-  }
-  .card-remove-button,
-  .card-copy-button
-  {
-    opacity: 0.1;
-    position: absolute;
-    bottom: 4px;
-    cursor: pointer;
-    animation: 300ms ;
-  }
-  .card-remove-button {
-    right: 8px;
-  }
-  .card-copy-button {
-    right: 32px;
-    bottom: -2px;
-    font-size: 2rem;
-  }
-  .card-remove-button:hover,
-  .card-copy-button:hover
-  {
-    opacity: 1;
-  }
-  video {
-    width: 160px;
-    height: 120px;
-  }
+}
 </style>
